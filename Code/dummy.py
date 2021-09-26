9f76aa9ff2250075f1d99274485d1212eee48b0e
JayaramGS <jayaramgs10@gmail.com>
2021-09-26 20:05:37 +0530

    Code

--- Code/dummy.py
+++ Code/dummy.py
@@ -13,6 +13,35 @@ class userDetails:
         if details[0][1] == self.user_name and details[0][2] == self.user_password:
             return details[0][0]
 
+    def personalDetails(self):
+        mycursor.execute("select * from user_details where user_id like %s", (user_id,))
+        details = mycursor.fetchall()
+        print('Username: \t\t', details[0][1])
+        print('E-mail ID: \t\t', details[0][2])
+        print('Phone Number: \t', details[0][3])
+        print('Password: \t\t', details[0][4])
+
+    def modifyDetails(self):
+        print("What do you want to update?")
+        print("1. Username\n2. Email-ID\n3. Phone_number\n4. Password")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            userName = str(input("Enter your new username: "))
+            passWord = str(input("To Confirm, please enter your password: "))
+            mycursor.execute('update user_details set user_name = replace(user_name,"userName") where user_password = "passWord"')
+            database.commit()
+
+class adminDetails:
+    def __init__(self, admin_name, admin_password):
+        self.admin_name = admin_name
+        self.admin_password = admin_password
+
+    def isAlreadyExistingAdmin(self):
+        mycursor.execute("select admin_id, admin_name, admin_password from admin_details where admin_name like %s",(self.admin_name,))
+        details = mycursor.fetchall()
+        if details[0][1] == self.admin_name and details[0][2] == self.admin_password:
+            return details[0][0]
+
 class displayAll:
     def list_planDetails(self):
         mycursor.execute("select * from plan_details")
@@ -25,13 +54,43 @@ class displayAll:
         print('Password_Protection   :',plan[0][5],'\t''\t''\t',plan[1][5],'\t''\t',plan[2][5])
         print('Storage_Capacity      :',plan[0][6],'\t''\t',plan[1][6],'\t''\t',plan[2][6])
         print('Show_Ads              :',plan[0][7],'\t''\t',plan[1][7],'\t''\t',plan[2][7])
-        print('\n')
-        print(plan_id[0],'Free')
-        print(plan_id[1],'Pro')
-        print(plan_id[2],'Business')
+
+class FileDetails:
+    def __init__(self):
+        self.user_id = user_id
+
+    def fileCreation(self):
+        file_name = str(input("Enter the file_name: "))
+        file_txt = input()
+        mycursor.execute("insert into file_details(file_id, file_name, file_txt) values(NULL,%s,%s)", (file_name, file_txt))
+        database.commit()
+
+    def fileDeletion(self):
+        file_name = str(input("Enter the file_name: "))
+        mycursor.execute("select file_name,file_txt from file_details where file_name like %s", (file_name,))
+        file = mycursor.fetchall()
+        print('File_Name',file[0][0])
+        print('',file[0][1])
+        print("Are you want to remove the file? ")
+        print("1. Yes")
+        print("2. No")
+        option = int(input("Enter your option: "))
+        if option == 1:
+            mycursor.execute("delete from file_details where file_name like %s", (file_name,))
+            database.commit()
+            print("File is removed.")
+        else:
+            print("No file is removed")
+
+    def fileContains(self):
+        file_name = str(input("Enter the file_name: "))
+        mycursor.execute("select file_name,file_txt from file_details where file_name like %s", (file_name,))
+        file = mycursor.fetchall()
+        print('File_Name: ', file[0][0])
+        print('', file[0][1])
 
 class plans:
-    def __init__(self,planId):
+    def __init__(self):
         self.planId = planId
 
 
@@ -41,29 +100,22 @@ class paymentDetails:
         self.planId = planId
 
     def payment(self):
-        count = 0
-        amount = 0
-        if planId == '2':
-            count += 1
-            amount += 1000
-        elif planId == '3':
-            count += 1
-            amount += 3000
-        else:
-            pass
-        card_number = input("Enter your Card_number: ")
-        mycursor.execute("insert into payment_details(payment_id,user_id,plan_id,card_number,count,amount) values(NULL,%s,%s,%s,%s,%s)",(user_id,planId,card_number,count,amount,))
-        database.commit()
 
+        if planId > 1:
+            card_number = str(input("Enter your Card_number: "))
+            mycursor.execute("insert into payment_details(payment_id,user_id,plan_id,card_number) values(NULL,%s,%s,%s)",(user_id,planId,card_number,))
+            database.commit()
 
 if __name__ == '__main__':
     print("Welcome to Kong-File Mail")
-    print("Sign_up")
-    print("Sign_in")
-    print("Admin")
-    destination = input("Enter your destination: ")
-    destination = destination.capitalize()
-    if destination == 'Sign_up':
+    print("\t1. Login")
+    print("\t2. Register")
+    print("\t3. Admin")
+    count = 0
+    amount = 0
+    destination = int(input("Enter your destination: "))
+    #destination = destination.lower()
+    if destination == 2:
         user_name = input("Enter your username: ")
         email_id = input("Enter your e-mail_id: ")
         phone_no = input("Enter your phone_number: ")
@@ -71,34 +123,128 @@ if __name__ == '__main__':
         mycursor.execute("insert into user_details(user_id, user_name, email_id, phone_no, user_password) values(NULL, %s, %s, %s, %s)",(user_name, email_id, phone_no, user_password,))
         database.commit()
         print("Sign Up Success..!!")
-        List_the_plans = displayAll()
-        List_the_plans.list_planDetails()
+        print("\n")
+        print("Do you want to...")
+        print("\t1. Create a file")
+        print("\t2. Remove a file")
+        print('\t3. View the file')
+        print('\t4. View the plans')
+        print('\t5. Logout')
+        option = int(input("Enter your choice: "))
+        if option == 1:
+           create = FileDetails()
+           create.fileCreation()
+        elif option == 2:
+            remove = FileDetails()
+            remove.fileDeletion()
 
-        planId = input("Enter the option to select the plan: ")
-        payments = paymentDetails()
-        payments.payment()
+        elif option == 3:
+            view = FileDetails()
+            view.fileContains()
+
+        elif option == 4:
+            List_the_plans = displayAll()
+            List_the_plans.list_planDetails()
+            print("Want to buy a plan?")
+            print('1.Pro')
+            print('2. Business')
+            planId =int(input("Enter the option to select the plan: "))
+            count = 0
+            amount = 0
+            if planId > 0 and planId < 2:
+                payments = paymentDetails()
+                payments.payment()
+                count += 1
+                amount += 1000
+            elif planId > 1 and planId < 3:
+                payments = paymentDetails()
+                payments.payment()
+                count += 1
+                amount += 3000
+            else:
+                exit()
+
+        elif option == 5:
+            print('Logged Out Successfully..!!!')
+            exit()
+
+        else:
+            print('Please Login and select correct option')
 
 
-    if destination == 'Sign_in':
+    if destination == 1:
         user_name = str(input("Enter your username: "))
         user_password = str(input("Enter your password: "))
+        user_name = user_name.capitalize()
         userValidation =  userDetails(user_name, user_password)
         user_id = userValidation.isAlreadyExistingUser()
         if user_id:
-            List_the_plans = displayAll()
-            List_the_plans.list_planDetails()
-            planId = input("Enter the option to select the plan: ")
-            payments = paymentDetails()
-            payments.payment()
+            print("Do you want to...")
+            print('\t1. Create a file')
+            print('\t2. Remove a file')
+            print('\t3. View the file')
+            print('\t4. View your personal details')
+            print('\t5. Modify your personal information')
+            print('\t6. View the plans')
+            print('\t7. Logout')
+            option = int(input("Enter your choice: "))
+            if option == 1:
+                create = FileDetails()
+                create.fileCreation()
+            elif option == 2:
+                remove = FileDetails()
+                remove.fileDeletion()
 
-    if destination == 'Admin':
-        print('1. Plans','\n','2. Records')
-        option = int(input("Enter your choice: "))
-        if option == 1:
-            List_the_plans = displayAll()
-            List_the_plans.list_planDetails()
-        if option == 2:
-            mycursor.execute("select count, amount from payment_details")
-            records = mycursor.fetchall()
-            print("Total Plans sold: ", records[0][0])
-            print("Total amount: ",'\t', records[0][1])
+            elif option == 3:
+                view = FileDetails()
+                view.fileContains()
+
+            elif option == 4:
+                persInform = userDetails(user_name,user_password)
+                persInform.personalDetails()
+
+            elif option == 5:
+                modifydetails = userDetails(user_name,user_password)
+                modifydetails.modifyDetails()
+
+            elif option == 6:
+                List_the_plans = displayAll()
+                List_the_plans.list_planDetails()
+                print("Want to buy a plan?")
+                planId = int(input("Enter the option to select the plan: "))
+                print('1.Pro')
+                print('2. Business')
+                if planId > 0 and planId < 2:
+                    payments = paymentDetails()
+                    payments.payment()
+                    count += 1
+                    amount += 1000
+                    mycursor.execute("insert into records(count,amount) values(%s,%s)",(count,amount))
+                    database.commit()
+                elif planId > 1 and planId < 3:
+                    payments = paymentDetails()
+                    payments.payment()
+                    count += 1
+                    amount += 3000
+                    mycursor.execute("insert into records(count,amount) values(%s,%s)", (count, amount))
+                    database.commit()
+                else:
+                    exit()
+
+
+    if destination == 3:
+        admin_name = str(input("Enter your username: "))
+        admin_password = str(input("Enter your password: "))
+        adminValidation = adminDetails(admin_name, admin_password)
+        admin_id = adminValidation.isAlreadyExistingAdmin()
+        if admin_id:
+            print('1. Plans''\t2. Records')
+            option = int(input("Enter your choice: "))
+            if option == 1:
+                List_the_plans = displayAll()
+                List_the_plans.list_planDetails()
+            if option == 2:
+                mycursor.execute("select * from records")
+                Records = mycursor.fetchall()
+                print("Total Plans sold: ", Records[0][0])
+                print("Total amount: \t", Records[0][1])
